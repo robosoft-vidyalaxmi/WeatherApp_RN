6524d74b306282a018c12808e27c3589caa92d24
Vidyalaxmi V Shenoy M <vidyalaxmiv.shenoy@robosoftin.com>
2025-07-28 20:19:07 +0530

    Remove storybook

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
-const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
 const config = getDefaultConfig(__dirname);
 
-// Wrap the config using Storybook's enhancer
-module.exports = withStorybook(config);
+module.exports = config;
